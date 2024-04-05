72e7142f3b575dcc4babf4d3e4fc86acc65fd985
Charef31 <charef.senouci31@gmail.com>
2024-04-05 10:17:46 +0200

    second commit

--- src/main/java/com/formation/DemoController.java
+++ src/main/java/com/formation/DemoController.java
@@ -1,5 +1,6 @@
 package com.formation;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,7 +8,7 @@ public class DemoController {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello World"
-;	}
+		return "Hello World";
+		}
 
 }
